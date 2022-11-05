f032d00234fa9bcb809edb6903a5cc26b2fd8549
Jonas Röger <jonas.roeger@tu-dortmund.de>
2022-11-05 19:21:36 +0100

    update instepct script

--- inspect_LARa_raw.py
+++ inspect_LARa_raw.py
@@ -1,11 +1,18 @@
-from common.data import LARa, LARaOptions
-import logging
-
-logging.getLogger().setLevel(logging.DEBUG)
+from common.data import LARa, LARaOptions, LARaClassLabelView, describeLARaLabels, ComposeTransforms
+from torch.utils.data import DataLoader
 
 validation_data = LARa(
   root='/home/jonas/Stuff/Datasets',
   window=300,
   stride=300,
+  transform=ComposeTransforms(LARaClassLabelView()),
   opts=[LARaOptions.ALL_RUNS, LARaOptions.ALL_SUBJECTS]
   )
+
+loader = DataLoader(dataset=validation_data, batch_size=64, shuffle=True)
+
+for batch, labels in loader:
+  print(labels.shape)
+  print(batch.shape)
+  print(describeLARaLabels(labels))
+  break
