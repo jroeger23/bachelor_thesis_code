cad375ed6063d4915075df434425715d331bda84
Jonas Röger <jonas.roeger@tu-dortmund.de>
2022-11-06 13:40:45 +0100

    change default logging handler

--- common/__init__.py
+++ common/__init__.py
@@ -9,7 +9,18 @@ logging.addLevelName(logging.WARNING, 'WRN')
 logging.addLevelName(logging.ERROR, 'ERR')
 logging.addLevelName(logging.FATAL, 'FTL')
 
-handler = logging.StreamHandler(sys.stdout)
+
+class MultilineHandler(logging.StreamHandler):
+    def __init__(self):
+        super(MultilineHandler, self).__init__()
+
+    def emit(self, record):
+        lines = record.msg.split('\n')
+        for l in lines:
+            record.msg = l
+            super(MultilineHandler, self).emit(record)
+
+handler = MultilineHandler()
 handler.setFormatter(logging.Formatter('[%(levelname)s][%(name)s] %(message)s'))
 
 
