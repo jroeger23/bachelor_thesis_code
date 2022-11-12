0b8de26c8182490d4289c8bd44beb89162f66f4c
Jonas Röger <jonas.roeger@tu-dortmund.de>
2022-11-13 00:46:49 +0100

    change lara training parameters

--- cnn-imu_lara.py
+++ cnn-imu_lara.py
@@ -14,42 +14,22 @@ def main():
   view = CombineViews(batch_view=LARaSplitIMUView(locations=LARaSplitIMUView.allLocations()),
                       labels_view=LARaClassLabelView())
 
-  train_data = LARa(download=True,
-                    window=100,
-                    stride=3,
-                    opts=[
-                        LARaOptions.ALL_SUBJECTS, LARaOptions.RUN01, LARaOptions.RUN02,
-                        LARaOptions.RUN03, LARaOptions.RUN04, LARaOptions.RUN05, LARaOptions.RUN06,
-                        LARaOptions.RUN07, LARaOptions.RUN08, LARaOptions.RUN11, LARaOptions.RUN12,
-                        LARaOptions.RUN13, LARaOptions.RUN14, LARaOptions.RUN15, LARaOptions.RUN16,
-                        LARaOptions.RUN17, LARaOptions.RUN18, LARaOptions.RUN21, LARaOptions.RUN22,
-                        LARaOptions.RUN23, LARaOptions.RUN24, LARaOptions.RUN25, LARaOptions.RUN26,
-                        LARaOptions.RUN27, LARaOptions.RUN28
-                    ],
-                    transform=ComposeTransforms([
-                        NaNToConstTransform(batch_constant=0, label_constant=7),
-                        ResampleTransform(freq_in=100, freq_out=30),
-                        LabelDtypeTransform(dtype=torch.int64)
-                    ]),
-                    view=view)
+  data = LARa(download=True,
+              window=100,
+              stride=20,
+              opts=[LARaOptions.ALL_RUNS, LARaOptions.ALL_SUBJECTS],
+              transform=ComposeTransforms([
+                  NaNToConstTransform(batch_constant=0, label_constant=7),
+                  ResampleTransform(freq_in=100, freq_out=30),
+                  LabelDtypeTransform(dtype=torch.int64)
+              ]),
+              view=view)
 
-  test_data = LARa(download=True,
-                   window=100,
-                   stride=3,
-                   opts=[
-                       LARaOptions.ALL_SUBJECTS, LARaOptions.RUN09, LARaOptions.RUN10,
-                       LARaOptions.RUN19, LARaOptions.RUN20, LARaOptions.RUN29, LARaOptions.RUN30
-                   ],
-                   transform=ComposeTransforms([
-                       NaNToConstTransform(batch_constant=0, label_constant=7),
-                       ResampleTransform(freq_in=100, freq_out=30),
-                       LabelDtypeTransform(dtype=torch.int64)
-                   ]),
-                   view=view)
-
-  train_data, validation_data = random_split(
-      dataset=train_data, lengths=[round(len(train_data) * 0.9),
-                                   round(len(train_data) * 0.1)])
+  train_data, validation_data, test_data = random_split(
+      dataset=data,
+      lengths=[round(len(data) * 0.80),
+               round(len(data) * 0.12),
+               round(len(data) * 0.08)])
 
   # Setup data loaders #############################################################################
   train_loader = DataLoader(dataset=train_data, batch_size=100, shuffle=True)
@@ -58,20 +38,20 @@ def main():
 
   # Setup model ####################################################################################
   imu_sizes = [segment.shape[1] for segment in train_data[0][0]]
-  model = CNNIMU(n_blocks=3, imu_sizes=imu_sizes, sample_length=100, n_classes=8)
-  trainer = pl.Trainer(max_epochs=3,
+  model = CNNIMU(n_blocks=2, imu_sizes=imu_sizes, sample_length=100, n_classes=8)
+  trainer = pl.Trainer(max_epochs=15,
                        accelerator='auto',
                        callbacks=[
                            pl_cb.DeviceStatsMonitor(),
                            pl_cb.LearningRateMonitor(),
                            pl_cb.EarlyStopping(monitor='validation/loss',
                                                min_delta=0.001,
-                                               patience=7,
+                                               patience=22,
                                                mode='min'),
                            MonitorWF1(),
                            MonitorAcc()
                        ],
-                       val_check_interval=1 / 5,
+                       val_check_interval=1 / 10,
                        enable_checkpointing=True,
                        profiler=pl_prof.AdvancedProfiler(dirpath="logs/CNNIMU-LARa",
                                                          filename="perf_logs"),
