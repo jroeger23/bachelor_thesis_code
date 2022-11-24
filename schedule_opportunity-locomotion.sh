1e0d054fae86b1472a137534c09f2cdaec059985
Jonas Röger <jonas.roeger@tu-dortmund.de>
2022-11-24 15:05:34 +0100

    add opp-loc schedule script

---
+++ schedule_opportunity-locomotion.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+batch_size="64,256"
+cnn_imu_blocks="2,3"
+cnn_imu_fc_features="256,512"
+optimizer="RMSProp,Adam"
+
+experiment='cnn-imu_opportunity-locomotion.py'
+
+generate_options() {
+  local IFS=' '
+  eval printf 'with\ %s,' "cnn_imu_blocks={$cnn_imu_blocks}\ cnn_imu_fc_features={$cnn_imu_fc_features}\ batch_size={$batch_size}\ optimizer={$optimizer}"
+}
+
+IFS=','
+for opt in $(generate_options); do 
+  IFS=' '
+  python $experiment $opt
+done
