a3ed2b7b72dba98a3892edbceb6be5e979ad1b29
Jonas Röger <jonas.roeger@tu-dortmund.de>
2022-12-07 15:36:52 +0100

    remove optimizer from opploc schedule script

--- schedule_opportunity-locomotion.sh
+++ schedule_opportunity-locomotion.sh
@@ -3,13 +3,12 @@
 batch_size="64,256"
 cnn_imu_blocks="2,3"
 cnn_imu_fc_features="256,512"
-optimizer="RMSProp,Adam"
 
 experiment='train_scripts/cnn-imu_opportunity-locomotion.py'
 
 generate_options() {
   local IFS=' '
-  eval printf 'with\ %s,' "cnn_imu_blocks={$cnn_imu_blocks}\ cnn_imu_fc_features={$cnn_imu_fc_features}\ batch_size={$batch_size}\ optimizer={$optimizer}"
+  eval printf 'with\ %s,' "cnn_imu_blocks={$cnn_imu_blocks}\ cnn_imu_fc_features={$cnn_imu_fc_features}\ batch_size={$batch_size}"
 }
 
 IFS=','
