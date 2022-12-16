bee3cb4c353bc62cb882d661dfe9165d2fac710a
Jonas Röger <jonas.roeger@tu-dortmund.de>
2022-12-16 15:45:01 +0100

    enable q_model torchscript save

--- train_scripts/ptsq_cnn-imu_lara.py
+++ train_scripts/ptsq_cnn-imu_lara.py
@@ -8,9 +8,9 @@ from pytorch_model_summary import summary
 from sacred.observers import MongoObserver
 
 from common.data import LARaDataModule
-from common.helper import checkpointsById, parseMongoConfig
+from common.helper import (checkpointsById, getRunCheckpointDirectory, parseMongoConfig)
 from common.model import CNNIMU
-from common.pl_components import MonitorAcc, MonitorBatchTime, MonitorWF1
+from common.pl_components import (MonitorAcc, MonitorBatchTime, MonitorWF1, SacredLogger)
 
 logger = logging.getLogger(__name__)
 
@@ -50,7 +50,7 @@ def defautltConfig():
 
 
 @ex.automain
-def main(trained_model_run_id: int, backend: str, batch_size: int):
+def main(trained_model_run_id: int, backend: str, batch_size: int, _run):
   # Data module
   data_module = LARaDataModule(batch_size=batch_size, window=100, stride=12, sample_frequency=30)
   data_module.setup('fit')
@@ -71,19 +71,26 @@ def main(trained_model_run_id: int, backend: str, batch_size: int):
 
   # Gather calibration data
   logger.info('Gathering activation statistics on train dataset')
-  trainer_statistics = pl.Trainer(logger=False, enable_checkpointing=False, accelerator='cpu')
+  trainer_statistics = pl.Trainer(logger=False,
+                                  enable_checkpointing=False,
+                                  limit_test_batches=0.05,
+                                  accelerator='cpu')
   trainer_statistics.test(model=fp32_model, dataloaders=data_module.train_dataloader())
 
-  # Convert to int8
-  logger.info('Converting to int8')
-  int8_model = tq.convert(module=fp32_model)
-  logger.info(summary(int8_model, data_module.test_set[0][0]))
+  # Convert to quantized
+  logger.info('Converting to quantized model')
+  q_model = tq.convert(module=fp32_model)
+  logger.info(summary(q_model, data_module.test_set[0][0]))
 
   # Test the quantized model
-  trainer_eval = pl.Trainer(logger=False,
+  trainer_eval = pl.Trainer(logger=SacredLogger(_run),
                             enable_checkpointing=False,
                             accelerator='cpu',
                             callbacks=[MonitorAcc(), MonitorBatchTime(),
                                        MonitorWF1()])
   logger.info('Testing int8 model')
-  trainer_eval.test(model=int8_model, dataloaders=data_module.test_dataloader())
+  trainer_eval.test(model=q_model, dataloaders=data_module.test_dataloader())
+
+  # Save quantized model
+  ckpt_dir = getRunCheckpointDirectory(root='./logs/checkpoints', _run=_run)
+  q_model.to_torchscript(file_path=ckpt_dir.joinpath('q_model.pt'))
