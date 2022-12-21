d4d233cee1b8bebe67856dbe7f6dd3b655a2b0a5
Jonas Röger <jonas.roeger@tu-dortmund.de>
2022-12-21 14:28:01 +0100

    add PTSQ lara config customization

--- train_scripts/ptsq_cnn-imu_lara.py
+++ train_scripts/ptsq_cnn-imu_lara.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 import incense
 import pytorch_lightning as pl
@@ -50,26 +50,36 @@ def defautltConfig():
   trained_model_run_id = bestRunId()
   backend = 'fbgemm'
   batch_size = 32
-  activation_observer = 'torch.ao.quantization.PlaceholderObserver'
-  activation_qscheme = 'torch.per_tensor_affine'
+  limit_calibration_set = None
+  n_bits = 7
+  activation_observer = 'torch.ao.quantization.HistogramObserver'
+  activation_qscheme = 'torch.per_channel_affine' if 'PerChannel' in activation_observer else 'torch.per_tensor_affine'
   activation_observer_args = {
-      'dtype': GlobalPlaceholder('torch.quint8'),
+      'dtype': GlobalPlaceholder('torch.quint8'),  # activation dtype must be quint8
       'quant_min': 0,
-      'quant_max': 2**7 - 1,
+      'quant_max': 2**n_bits - 1,
       'qscheme': GlobalPlaceholder(activation_qscheme)
   }
+
+  weight_range = 'full'  # full, uint or symmetric
   weight_observer = 'torch.ao.quantization.PerChannelMinMaxObserver'
-  weight_qscheme = 'torch.per_tensor_symmetric'
+  weight_qscheme = f'torch.{"per_channel" if "PerChannel" in weight_observer else "per_tensor"}_{"symmetric" if weight_range == "symmetric" else "affine"}'
   weight_observer_args = {
-      'dtype': GlobalPlaceholder('torch.qint8'),
-      'quant_min': -2**6,
-      'quant_max': 2**6 - 1,
-      'qscheme': GlobalPlaceholder(weight_qscheme)
+      'dtype':
+          GlobalPlaceholder('torch.qint8'),  # weight dtype must be qint8
+      'quant_min':
+          0 if weight_range == 'uint' else
+          -2**(n_bits - 1) if weight_range == 'full' else -2**(n_bits - 1) + 1,
+      'quant_max':
+          2**(n_bits - 1) - 1,
+      'qscheme':
+          GlobalPlaceholder(weight_qscheme)
   }
 
 
 @ex.automain
-def main(trained_model_run_id: int, backend: str, batch_size: int, activation_observer: str,
+def main(trained_model_run_id: int, backend: str, batch_size: int,
+         limit_calibration_set: Optional[float], activation_observer: str,
          activation_observer_args: Dict[str, Any], weight_observer: str,
          weight_observer_args: Dict[str, Any], _run):
   torch.backends.quantized.engine = backend
@@ -99,12 +109,13 @@ def main(trained_model_run_id: int, backend: str, batch_size: int, activation_ob
   logger.info('Gathering activation statistics on train dataset')
   trainer_statistics = pl.Trainer(logger=False,
                                   enable_checkpointing=False,
-                                  limit_test_batches=0.05,
-                                  accelerator='cpu')
+                                  limit_test_batches=limit_calibration_set,
+                                  accelerator='auto')
   trainer_statistics.test(model=fp32_model, dataloaders=data_module.train_dataloader())
 
   # Convert to quantized
   logger.info('Converting to quantized model')
+  fp32_model.to('cpu')
   q_model = tq.convert(module=fp32_model)
   logger.info(summary(q_model, data_module.test_set[0][0]))
 
@@ -120,8 +131,3 @@ def main(trained_model_run_id: int, backend: str, batch_size: int, activation_ob
   # Save quantized model
   ckpt_path = getRunCheckpointDirectory(root='./logs/checkpoints', _run=_run).joinpath('model.ckpt')
   trainer_eval.save_checkpoint(filepath=ckpt_path)
-
-  # Try to load the model
-  logger.info('Testing loaded quantized model')
-  loaded_model = CNNIMU.load_from_checkpoint(ckpt_path)
-  trainer_eval.test(model=loaded_model, dataloaders=data_module.test_dataloader())
