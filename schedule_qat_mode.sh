ab4db67741cc68e1468d5bea7290eea63be9c4b2
Jonas Röger <jonas.roeger@tu-dortmund.de>
2023-01-19 18:02:58 +0100

    fix qat schedule script param naming

--- schedule_qat_mode.sh
+++ schedule_qat_mode.sh
@@ -4,14 +4,14 @@ n_bits="7,"
 activation_observer="torch.ao.quantization.HistogramObserver,torch.ao.quantization.MinMaxObserver,torch.ao.quantization.MovingAverageMinMaxObserver"
 weight_observer="torch.ao.quantization.MinMaxObserver,torch.ao.quantization.PerChannelMinMaxObserver"
 weight_range="full,"
-dataset="pamap2,lara,opportunity"
+use_dataset="pamap2,lara,opportunity"
 
 
 experiment="train_scripts/qat_cnn-imu.py"
 
 generate_options() {
   local IFS=' '
-  eval printf 'with\ %s,' "n_bits={$n_bits}\ activation_observer={$activation_observer}\ weight_observer={$weight_observer}\ weight_range={$weight_range}\ dataset={$dataset}"
+  eval printf 'with\ %s,' "n_bits={$n_bits}\ activation_observer={$activation_observer}\ weight_observer={$weight_observer}\ weight_range={$weight_range}\ use_dataset={$use_dataset}"
 }
 
 IFS=','
