913a71d5f71e98f355efb0b48aa6c6c3d283ed0e
Jonas Röger <jonas.roeger@tu-dortmund.de>
2023-01-19 16:23:40 +0100

    add Opportunity DataLoader

--- common/data/__init__.py
+++ common/data/__init__.py
@@ -1,15 +1,14 @@
-from .common import (CombineViews, ComposeTransforms, LabelDtypeTransform, NaNToConstTransform,
-                     ResampleTransform, BatchAdditiveGaussianNoise, RangeNormalize,
-                     MeanVarianceNormalize, ClipSampleRange, RemoveNanRows, BlankInvalidColumns)
+from .common import (BatchAdditiveGaussianNoise, BlankInvalidColumns, ClipSampleRange, CombineViews,
+                     ComposeTransforms, LabelDtypeTransform, MeanVarianceNormalize,
+                     NaNToConstTransform, RangeNormalize, RemoveNanRows, ResampleTransform)
+from .data_modules import (LARaDataModule, OpportunityDataModule, Pamap2DataModule)
 from .lara import (LARa, LARaClassLabelView, LARaDataView, LARaIMUView, LARaLabelsView, LARaOptions,
                    LARaSplitIMUView, describeLARaLabels)
 from .mnist import *
 from .opportunity import (Opportunity, OpportunityHumanSensorUnitsView, OpportunityLabelView,
                           OpportunityLocomotionLabelAdjustMissing3, OpportunityLocomotionLabelView,
-                          OpportunityOptions, OpportunitySensorUnitView, OpportunitySplitLabelView,
-                          OpportunityRemoveHumanSensorUnitNaNRows, OpportunitySplitSensorUnitsView,
-                          allOpportunityLabels, describeLabels)
-from .pamap2 import (Pamap2, Pamap2IMUView, Pamap2Options, Pamap2SplitIMUView, Pamap2View,
-                     describePamap2Labels, Pamap2FilterRowsByLabel, Pamap2InterpolateHeartrate)
-
-from .data_modules import LARaDataModule, Pamap2DataModule
+                          OpportunityOptions, OpportunityRemoveHumanSensorUnitNaNRows,
+                          OpportunitySensorUnitView, OpportunitySplitLabelView,
+                          OpportunitySplitSensorUnitsView, allOpportunityLabels, describeLabels)
+from .pamap2 import (Pamap2, Pamap2FilterRowsByLabel, Pamap2IMUView, Pamap2InterpolateHeartrate,
+                     Pamap2Options, Pamap2SplitIMUView, Pamap2View, describePamap2Labels)
--- common/data/data_modules.py
+++ common/data/data_modules.py
@@ -1,13 +1,18 @@
+from typing import Optional
+
 import pytorch_lightning as pl
-
-from .lara import LARa, LARaSplitIMUView, LARaClassLabelView, LARaOptions
-from .common import Transform, View, ComposeTransforms, BatchAdditiveGaussianNoise, RemoveNanRows, ResampleTransform, LabelDtypeTransform, MeanVarianceNormalize, ClipSampleRange, CombineViews
-from .pamap2 import Pamap2InterpolateHeartrate, Pamap2SplitIMUView, Pamap2FilterRowsByLabel, Pamap2, Pamap2Options
-
 import torch
 from torch.utils.data import DataLoader, Dataset
 
-from typing import Optional
+from .common import (BatchAdditiveGaussianNoise, BlankInvalidColumns, ClipSampleRange, CombineViews,
+                     ComposeTransforms, LabelDtypeTransform, MeanVarianceNormalize, RemoveNanRows,
+                     ResampleTransform, Transform, View)
+from .lara import LARa, LARaClassLabelView, LARaOptions, LARaSplitIMUView
+from .opportunity import (Opportunity, OpportunityHumanSensorUnitsView,
+                          OpportunityLocomotionLabelAdjustMissing3, OpportunityLocomotionLabelView,
+                          OpportunityOptions, OpportunityRemoveHumanSensorUnitNaNRows)
+from .pamap2 import (Pamap2, Pamap2FilterRowsByLabel, Pamap2InterpolateHeartrate, Pamap2Options,
+                     Pamap2SplitIMUView)
 
 default_lara_dynamic_transform = ComposeTransforms([BatchAdditiveGaussianNoise(mu=0, sigma=0.01)])
 default_lara_view = CombineViews(
@@ -40,6 +45,22 @@ def default_pamap2_static_transform(use_transient_class: bool, sample_frequency:
       LabelDtypeTransform(dtype=torch.int64),
       MeanVarianceNormalize(mean=0.5, variance=1)
   ])
+
+
+def default_opportunity_static_transform(blank_invalid_columns: bool) -> Transform:
+  return ComposeTransforms([
+      BlankInvalidColumns(sample_thresh=0.5 if blank_invalid_columns else None, label_thresh=None),
+      OpportunityRemoveHumanSensorUnitNaNRows(),
+      LabelDtypeTransform(dtype=torch.int64),
+      OpportunityLocomotionLabelAdjustMissing3(),
+      MeanVarianceNormalize(mean=0.5, variance=1),
+  ])
+
+
+default_opportunity_dynamic_transform = ComposeTransforms(
+    [BatchAdditiveGaussianNoise(mu=0, sigma=0.01)])
+default_opportunity_view = CombineViews(sample_view=OpportunityHumanSensorUnitsView(),
+                                        label_view=OpportunityLocomotionLabelView())
 
 
 class Pamap2DataModule(pl.LightningDataModule):
@@ -223,6 +244,99 @@ class LARaDataModule(pl.LightningDataModule):
                            static_transform=self.static_transform,
                            dynamic_transform=self.dynamic_transform,
                            view=self.view)
+
+  def train_dataloader(self, **dataloader_kwargs) -> DataLoader:
+    kwargs = {'shuffle': True} | dataloader_kwargs
+    return DataLoader(dataset=self.train_set, batch_size=self.batch_size, **kwargs)
+
+  def val_dataloader(self, **dataloader_kwargs) -> DataLoader:
+    kwargs = {'shuffle': False} | dataloader_kwargs
+    return DataLoader(dataset=self.val_set, batch_size=self.batch_size, **kwargs)
+
+  def test_dataloader(self, **dataloader_kwargs) -> DataLoader:
+    kwargs = {'shuffle': False} | dataloader_kwargs
+    return DataLoader(dataset=self.test_set, batch_size=self.batch_size, **kwargs)
+
+
+class OpportunityDataModule(pl.LightningDataModule):
+
+  def __init__(self,
+               batch_size: int,
+               window: int,
+               stride: int,
+               blank_invalid_columns: Optional[bool],
+               static_transform: Optional[Transform] = None,
+               dynamic_transform: Optional[Transform] = None,
+               view: Optional[View] = None) -> None:
+    super().__init__()
+    if static_transform is None and blank_invalid_columns is None:
+      raise ValueError(f'Using default static_transform, but blank_invalid_columns is None')
+
+    self.batch_size = batch_size
+    self.window = window
+    self.stride = stride
+
+    self.static_transform = static_transform or default_opportunity_static_transform(
+        blank_invalid_columns or True)
+    self.dynamic_transform = dynamic_transform or default_opportunity_dynamic_transform
+    self.view = view or default_opportunity_view
+
+    self._train_set: Optional[Dataset] = None
+    self._val_set: Optional[Dataset] = None
+    self._test_set: Optional[Dataset] = None
+
+  @property
+  def train_set(self) -> Dataset:
+    assert self._train_set is not None
+    return self._train_set
+
+  @train_set.setter
+  def train_set(self, value) -> None:
+    self._train_set = value
+
+  @property
+  def val_set(self) -> Dataset:
+    assert self._val_set is not None
+    return self._val_set
+
+  @val_set.setter
+  def val_set(self, value) -> None:
+    self._val_set = value
+
+  @property
+  def test_set(self) -> Dataset:
+    assert self._test_set is not None
+    return self._test_set
+
+  @test_set.setter
+  def test_set(self, value) -> None:
+    self._test_set = value
+
+  def setup(self, stage: str) -> None:
+    if stage == 'fit':
+      self.train_set = Opportunity(download=True,
+                                   window=self.window,
+                                   stride=self.stride,
+                                   opts=[OpportunityOptions.DEFAULT_TRAIN],
+                                   static_transform=self.static_transform,
+                                   dynamic_transform=self.dynamic_transform,
+                                   view=self.view)
+    elif stage == 'validate':
+      self.val_set = Opportunity(download=True,
+                                 window=self.window,
+                                 stride=self.stride,
+                                 opts=[OpportunityOptions.DEFAULT_VALIDATION],
+                                 static_transform=self.static_transform,
+                                 dynamic_transform=self.dynamic_transform,
+                                 view=self.view)
+    elif stage == 'test':
+      self.test_set = Opportunity(download=True,
+                                  window=self.window,
+                                  stride=self.stride,
+                                  opts=[OpportunityOptions.DEFAULT_TEST],
+                                  static_transform=self.static_transform,
+                                  dynamic_transform=self.dynamic_transform,
+                                  view=self.view)
 
   def train_dataloader(self, **dataloader_kwargs) -> DataLoader:
     kwargs = {'shuffle': True} | dataloader_kwargs
