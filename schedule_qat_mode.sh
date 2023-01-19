083441036756cfa03e974475ca2501a862ee0609
Jonas Röger <jonas.roeger@tu-dortmund.de>
2023-01-19 19:51:09 +0100

    fix schedule_qat_mode

--- schedule_qat_mode.sh
+++ schedule_qat_mode.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-n_bits="7,"
 activation_observer="torch.ao.quantization.HistogramObserver,torch.ao.quantization.MinMaxObserver,torch.ao.quantization.MovingAverageMinMaxObserver"
 weight_observer="torch.ao.quantization.MinMaxObserver,torch.ao.quantization.PerChannelMinMaxObserver"
-weight_range="full,"
 use_dataset="pamap2,lara,opportunity"
 
 
@@ -11,7 +9,7 @@ experiment="train_scripts/qat_cnn-imu.py"
 
 generate_options() {
   local IFS=' '
-  eval printf 'with\ %s,' "n_bits={$n_bits}\ activation_observer={$activation_observer}\ weight_observer={$weight_observer}\ weight_range={$weight_range}\ use_dataset={$use_dataset}"
+  eval printf 'with\ %s,' "n_bits=7\ activation_observer={$activation_observer}\ weight_observer={$weight_observer}\ weight_range=full\ use_dataset={$use_dataset}"
 }
 
 IFS=','
